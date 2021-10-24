944225c7adfe0eabdba82cf9557e3c74033cc79e
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-10-24 20:21:52 -0300

    .

---
+++ 6° função.py
@@ -0,0 +1,40 @@
+def adiciona_na_mesa(piece, table):
+
+    last_peace = len(table)-1
+    inv_piece = [0,0]
+
+    # se a mesa estiver vazia
+    if table == []:
+        table.append(piece)
+
+    # se a peça escolhida encaixar na peça do início da lista
+    # mas tiver que inverter
+    elif piece[0] == table[0][0]:
+        inv_piece[0] = piece[1]
+        inv_piece[1] = piece[0]
+        new_table = [inv_piece]
+        for p in table:
+            new_table.append(p)
+        table = new_table
+
+    # se a peça escolhida encaixar na peça do início da lista
+    elif piece[1] == table[0][0]:
+        new_table = [piece]
+        for p in table:
+            new_table.append(p)
+        table = new_table
+
+    # se a peça escolhida encaixar na peça do final da lista
+    elif piece[0] == table[last_peace][1]:
+        table.append(piece)
+
+     # se a peça escolhida encaixar na peça do final da lista
+     # mas tiver que inverter
+    elif piece[1] == table[last_peace][1]:
+        inv_piece[0] = piece[1]
+        inv_piece[1] = piece[0]
+        table.append(inv_piece)
+    
+    
+
+    return table
