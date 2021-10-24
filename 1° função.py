78451f790996b336746f0a3c6d479c314c225774
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-10-24 20:51:42 -0300

    .

--- 1° função.py
+++ 1° função.py
@@ -1,8 +1,15 @@
 from random import shuffle
 
-def cria_pecas(lista):
-   
-    return lista
-    
+def make_pieces(l_pieces):
 
+    # cria as peças do dominó
+    for n1 in range(0,7):
+        for n2 in range(0,7):
+            if [n2,n1] not in l_pieces: #impede que surjam peças iguais
+                l_pieces.append([n1,n2])
+
+    shuffle(l_pieces)
+
+    return l_pieces
+    
     
