9ea8f254ea2422c274b5212049e9744f129689e6
Isabella Amorim <isabellasa@al.insper.edu.br>
2021-10-29 11:06:57 -0300

    adiciona funçao 5 na biblioteca

--- biblioteca_funções.py
+++ biblioteca_funções.py
@@ -116,4 +116,32 @@ def soma_pecas(pecas):
         pontos += peca[0] + peca[1]
 
     return pontos
+
+#Função 5: verifica as possibilidades de peças que podem ser jogadas por rodada. 
+def posicoes_possiveis(table, player_pieces):
+
+    possibilities = []# lista que recebe os valores de retorno da função
+
+    # possibilidades de jogo em mesa sem peça
+    if table == []:
+        i = 0
+        while i < len(player_pieces):
+            possibilities.append(i)
+            i += 1
+
+    # possibilidades de jogo em mesa com pelo menos uma peça
+    else:
+        # números na estremidade da mesa
+        num1 = table[0][0]
+        num2 = table[len(table)-1][1]
+
+        # caucula as posições das peças jogáveis
+        i = 0
+        for piece in player_pieces:
+
+            if num1 in piece or num2 in piece:
+                possibilities.append(i)
+            i += 1
+
+    return possibilities
     
