408d8c27e8b6a3516651cf58a5e93ca172019c3c
Isabella Amorim <isabellasa@al.insper.edu.br>
2021-10-29 11:29:26 -0300

    primeiro teste de jogo

---
+++ jogo_teste1.py
@@ -0,0 +1,7 @@
+import biblioteca_funções
+
+print('\033[47m'+'\033[1m'+'\033[30m'+' Bem vindo ao JOGO DE DOMINÓ '+'\033[0;0m')
+print('O objetivo é ser o primeiro jogador a ficar sem peças na mão!')
+print()
+print('Vamos começar.')
+n_jogadores = int(input('Escolha entre 2, 3 ou 4 jogadores: '))
