522d87451d1105328867b21200710f80a60b31c5
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-11-02 10:00:41 -0300

    numero de jogadores loop

--- jogo_teste1.py
+++ jogo_teste1.py
@@ -1,6 +1,6 @@
 from biblioteca_funções import * 
 
-#Boas vindas
+# Boas vindas
 print('\033[47m'+'\033[1m'+'\033[30m'+' Bem vindo ao JOGO DE DOMINÓ '+'\033[0;0m')
 print('O objetivo é ser o primeiro jogador a ficar sem peças na mão!')
 print()
@@ -8,15 +8,25 @@ print('Vamos começar.')
 n_jogadores = int(input('Escolha entre 2, 3 ou 4 jogadores: '))
 print()
 
-#Criação e distribuição de peças
+while n_jogadores < 2 or n_jogadores > 4:
+    print('Número de jogadores inválido.')
+    n_jogadores = int(input('Escolha entre 2, 3 ou 4 jogadores: '))
+print()
+
+# Criação a distribuição de peças
 pecas = cria_pecas()
 dicionario_pecas = inicia_jogo(n_jogadores, pecas)
 
-#Mostra peças ao jogador 0 
-print('Essas são suas peças:')
-print(dicionario_pecas['jogadores'][0])
+# Cria ordem de jogadas aleatório
+ordem_jogadores = embaralha_jogaderes(n_jogadores)
 
-#Define e printa a mesa inicial 
-mesa = []
-print()
-print('MESA: {}'.format(mesa))
+# loop q se repete até um jogador não ter peças
+while 0 == 0:
+    # Mostra peças ao jogador 0 
+    print('Essas são suas peças:')
+    print(dicionario_pecas['jogadores'][0])
+
+    # Define e printa a mesa inicial 
+    mesa = []
+    print()
+    print('MESA: {}'.format(mesa))
